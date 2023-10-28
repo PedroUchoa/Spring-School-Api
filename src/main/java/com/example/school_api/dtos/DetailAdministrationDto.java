ba73778e354d84d7c86e986a54adff85b551aa85
João Pedro Uchôa <joaopedrouchoacampos@gmail.com>
2023-10-28 20:57:41 -0300

    Update: AdministrationDto

--- src/main/java/com/example/school_api/dtos/DetailAdministrationDto.java
+++ src/main/java/com/example/school_api/dtos/DetailAdministrationDto.java
@@ -1,12 +1,13 @@
 package com.example.school_api.dtos;
 
 import com.example.school_api.domain.Administration;
+import com.example.school_api.enums.AdministrationFunctions;
 
 import java.time.LocalDateTime;
 
 public record DetailAdministrationDto(String id, String name, Double Salary,
                                       String phone, LocalDateTime startDate,
-                                      LocalDateTime endDate, Boolean isActive,String functional) {
+                                      LocalDateTime endDate, Boolean isActive, AdministrationFunctions functional) {
 
     public DetailAdministrationDto(Administration administration){
         this(administration.getId(), administration.getName(), administration.getSalary(), administration.getPhone(), administration.getStartDate(),administration.getEndDate(),administration.getIsActive(),administration.getFunctional());
